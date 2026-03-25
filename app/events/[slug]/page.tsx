b95aba01d58fc7c2173cd7f9fed0349242f09a13
modar <modarbalabdllah660@gmail.com>
2026-03-25 20:05:40 +0300

    dev-event

---
+++ app/events/[slug]/page.tsx
@@ -0,0 +1,112 @@
+import { notFound } from "next/navigation";
+import Image from "next/image";
+import Bookevent from "@/components/Bookevent"; 
+import { IEvent } from "@/database/event.model";
+import { getSimilarEventBySlug } from "@/lib/actions/event.actions";
+import EventCard from "@/components/EventCard";
+const BASE_URL = process.env.BASE_URL || 'http://localhost:3000';
+const EventDetailItem = ({ icon, alt, label }: { icon: string; alt: string; label: string; }) => (
+    <div className="flex items-center gap-2">
+        <Image src={icon} alt={alt} width={17} height={17} />
+        <p>{label}</p>
+    </div>
+)
+const EventAgenda = ({ agendaItems }: { agendaItems: string[] }) => (
+    <div className="agenda">
+        <h2>Agenda</h2>
+        <ul>
+            {agendaItems.map((item) => (
+                <li key={item}>{item}</li>
+            ))}
+        </ul>
+    </div>
+)
+const EventTags = ({ tags }: { tags?: string[] | string; }) => {
+    if (!tags) return null;
+    const tagsString = Array.isArray(tags) ? tags[0] : tags;
+    if (!tagsString) return null;
+
+    const tagsArray = tagsString.replace(/[\[\],]/g, '').trim().split(/\s+/);
+    
+    return (
+        <div className="flex flex-row gap-1.5 flex-wrap">
+            {tagsArray.map((tag, index) => (
+                <div className="pill" key={index}>{tag}</div>
+            ))}
+        </div>
+    );
+};
+const page = async ({params}:{params :Promise<{slug: string}>}) => {
+  const {slug} = await params;
+  const request =await fetch(`${BASE_URL}/api/events/${slug}`);
+  const {event:{description,image,overview,date,time,location,mode,audience,agenda,organizer,tags}} = await request.json();
+
+  if(!description)
+    return notFound();
+ 
+  const booking=10;
+  const smiliarEvents: IEvent[] = await getSimilarEventBySlug(slug);
+
+  return (
+    <section id="event">
+        <div className="header">
+            <h1>Event Description</h1>
+            <p className="mt-2">{description}</p>
+        </div>
+        <div className="details">
+            {/*left side -Event content*/}
+            <div className="content">
+            <Image src={image} alt="Event Banner" width={800} height={800} className="banner"/>
+            <section className="flex-col gap-2">
+              <h2>Overview</h2>
+              <p>{overview}</p>
+            </section>
+            <section className="flex-col gap-2">
+              <h2>Event Detalis</h2> 
+
+        <EventDetailItem icon="/icons/calendar.svg" alt="calendar" label={date} />
+                        <EventDetailItem icon="/icons/clock.svg" alt="clock" label={time} />
+                        <EventDetailItem icon="/icons/pin.svg" alt="pin" label={location} />
+                        <EventDetailItem icon="/icons/mode.svg" alt="mode" label={mode} />
+                        <EventDetailItem icon="/icons/audience.svg" alt="audience" label={audience} />
+            </section>
+            <EventAgenda agendaItems={agenda} />
+            <section className="flex-col gap-2">
+              <h2>Organizer</h2>
+              <p>{organizer}</p>
+            </section>
+            <EventTags tags={tags} />
+            </div>
+
+
+
+            {/*Right side -Event content*/}
+            <aside className="booking"> 
+                <div className="Singup-card">
+                    <h2 >Book Your Spot</h2>
+                    {booking > 0 ? (
+                        <p className="text-sm mt-2">{booking} people who have already booked their spot!</p>
+                    ) : (
+                        <p className="text-sm">Be the first one to book your spot!</p>
+                    )}
+                    <Bookevent />
+
+                </div>
+            </aside>
+        </div>
+        <div className="flex w-full flex-col gap-4 pt-20">
+            <h2 className="text-2xl font-bold">Similar Events</h2>
+            <div>
+                {smiliarEvents.length > 0 && smiliarEvents.map(( smiliarEvent: IEvent) => (
+                    <EventCard {...smiliarEvent} key={smiliarEvent.title} /> 
+
+                ))}
+            </div>
+        </div>
+        
+    
+    </section>
+  )
+}
+
+export default page
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
-import {events} from "@/lib/constants"
-export default function Home() {
+import { IEvent } from "@/database";
+import { cacheLife } from "next/cache";
+
+const Base_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+export default async function Home() {
+  'use cache';
+  cacheLife('hours')
+ const response = await fetch(`${Base_URL}/api/events`);
+ const { events } = await response.json();
   return (
         <section className="space-y-6">
             <h1 className="text-center">{/* eslint-disable-next-line react/no-unescaped-entities */}
@@ -11,7 +18,7 @@ export default function Home() {
     <div className="mt-20 space-y-7">
     <h3>Featured Events</h3>
       <ul className="events">
-        {events.map((event) => (
+        {events&&events.length > 0 && events.map((event: IEvent ) => (
         <li key={event.title}>
           <EventCard{...event}/>
         </li>
